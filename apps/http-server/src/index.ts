8bcbb3b4f77aa05bfeeb579bf670d23adebb0c22
MrBlackGhostt <hemantkumar4213@icloud.com>
2025-02-09 13:10:31 +0530

    first commit

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,27 @@
+import express from "Express";
+import { client } from "@repo/db/client";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Sending the Get req");
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await client.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+  res.json({
+    message: "Signup Successfull",
+    id: user.id,
+  });
+});
+
+app.listen(3002, () => {
+  console.log("Server running on 3000");
+});
---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,36 @@
+import { WebSocketServer } from "ws";
+import { client } from "@repo/db/client";
+
+const server = new WebSocketServer({ port: 3001 });
+
+server.on("connection", (socket) => {
+  console.log("New client connected");
+
+  // Error handling
+  socket.on("error", (err) => {
+    console.error("WebSocket error:", err);
+  });
+
+  // Save user to DB
+  client.user
+    .create({
+      data: {
+        username: Math.random().toString(),
+        password: Math.random().toString(),
+      },
+    })
+    .then(() => console.log("User created"))
+    .catch((err) => console.error("DB Error:", err));
+
+  socket.send("Connected to 3001");
+
+  socket.on("message", (message) => {
+    console.log("Received:", message.toString());
+  });
+
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+console.log("WebSocket server running on ws://localhost:3001");
